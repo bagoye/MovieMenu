b2f8aa40e9bd7996af6661edbef79dbf36365cb0
seondy <seondy99@gmail.com>
2023-05-20 19:34:38 +0900

    0520 temp upload

--- final_pjt_front/src/App.vue
+++ final_pjt_front/src/App.vue
@@ -1,20 +1,21 @@
 <template>
   <div id="app">
     <nav>
-      <img src="./assets/dayoungsuyeom.png" alt="logo" @click="toHome">
+      <img src="./assets/dayoungsuyeom.png" class="logo-img" alt="logo" @click="toHome">
 
-      <div>
-        <router-link :to="{name: 'HomeView'}">HOME</router-link> |
-        <router-link :to="{name: 'RandomMovieView'}">오늘의 추천</router-link> |
-        <router-link :to="{name: 'CommunityView'}">커뮤니티</router-link> |
-        <router-link :to="{name: 'AccountView'}">Login</router-link>
+      <div class="nav-link">
+        <router-link :to="{name: 'HomeView'}">홈</router-link>
+        <router-link :to="{name: 'RandomMovieView'}">오늘의 추천</router-link>
+        <router-link :to="{name: 'CommunityView'}">커뮤니티</router-link>
+        <router-link :to="{name: 'AccountView'}">로그인</router-link>
         <!-- 회원가입은 로그인 페이지 들어가있을 때 보일거임 -->
         <!-- <router-link to="/">Logout</router-link> | -->
         <!-- 로그아웃, 로그인은 로그인 돼있을 때 떠야해서 v-if로 조건 줘야할듯 -->
-        <a href="#"><img src="#" alt="profile image">프로필</a>
+        <a href="#" class="profile-btn"><img src="#" alt="profile image" v-if="img">dd</a>
       </div>
 
     </nav>
+    <div class="divide-block"></div>
     <router-view/>
   </div>
 </template>
@@ -31,5 +32,51 @@ export default {
 </script>
 
 <style>
+body {
+  background-color: #FFFBEF;
+}
 
+#app {
+  width: 1094px;
+  margin: 0 auto;
+}
+
+.logo-img {
+  height: 45%;
+  cursor: pointer;
+}
+
+nav {
+  width: 1094px;
+  height: 150px;
+  display: flex;
+  align-items: center;
+
+  position: fixed;
+  top: 0;
+}
+
+.divide-block {
+  width: 100%;
+  height: 150px;
+}
+
+.nav-link {
+  width: 100%;
+  text-align: right;
+}
+
+.nav-link * {
+  margin-left: 40px;
+  font-size: 16px;
+  color: #000;
+  text-decoration: none;
+}
+
+.profile-btn {
+  width: 36px;
+  height: 36px;
+  background-color: #ccc;
+  border-radius: 50%;
+}
 </style>
--- final_pjt_front/src/views/Movies/HomeView.vue
+++ final_pjt_front/src/views/Movies/HomeView.vue
@@ -1,10 +1,16 @@
 <template>
-  <div>
-    <h1>오늘의 밥 친구를 찾아서 가보자굿 -0- ;;;;; ;; bb</h1>
-    <div>
-      <button @click="toChooseView">오늘의 밥친구는 ?! 얘는 로그인 했을 때만 가능</button>
-      <button @click="toNormalMovieView">로그인 안해도 상관 X => 개무난한 추천페이지</button>
+  <div class="home-view">
+    <div class="menu-out">
+      <div class="menu-in">
+        <h1 class="h1-tag">오늘의 밥 친구를 찾아서 가보자굿 -0- ;;;;; ;; bb</h1>
+        <div>
+          <button @click="toChooseView">오늘의 밥친구는 ?! 얘는 로그인 했을 때만 가능</button>
+          <button @click="toNormalMovieView">로그인 안해도 상관 X => 개무난한 추천페이지</button>
+        </div>
+      </div>
     </div>
+
+
   </div>
 </template>
 
@@ -25,5 +31,25 @@ export default {
 </script>
 
 <style>
+.home-view {
+  width: 100%;
+  text-align: center;
+}
 
+.menu-out {
+  width: 100%;
+  height: 500px;
+  background-color: #D6E7F6;
+  border-radius: 20px 20px 0 0;
+  position: relative;
+}
+.menu-in {
+  width: 90%;
+  height: 90%;
+  position: absolute;
+  left: 5%;
+  bottom: 0;
+  border-radius: 20px 20px 0 0;
+  background-color: #fff;
+}
 </style>
