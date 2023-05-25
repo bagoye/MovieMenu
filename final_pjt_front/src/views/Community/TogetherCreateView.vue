fcf406e41cfc77c38d91b64515fa792bbe4ae910
seondy <seondy99@gmail.com>
2023-05-26 06:16:22 +0900

    temp CSS update

--- final_pjt_front/src/views/Community/TogetherCreateView.vue
+++ final_pjt_front/src/views/Community/TogetherCreateView.vue
@@ -1,28 +1,34 @@
 <template>
-  <div class="together-create row">
-    <h1>같이 봐요 게시글 작성</h1>
+  <div class="together-create">
+    <h1>영화 같이 봐요 <span class="text-blue">작성하기</span></h1>
 
-    <div class="article-form col-sm-12 col-lg-6">
-      <form @submit.prevent="createArticle">
-        <label for="title">제목 : </label>
-        <input type="text" id="title" v-model.trim="title"><br>
-        <label for="content">내용 : </label>
-        <textarea id="content" cols="30" rows="10" v-model="content"></textarea><br>
-        <input type="submit" id="submit">
-      </form>
-    </div>
+    <div class="together-create-wrap row">
+      <div class="article-form col-sm-12 col-lg-6">
+        <form @submit.prevent="createArticle">
+          <div>
+            <label for="title"><b>제목</b></label><br>
+            <input type="text" class="mt-2" id="title" v-model.trim="title" placeholder="제목을 작성해주세요!"><br>
+          </div>
+          <div>
+            <label for="content" class="mt-4"><b>내용</b></label><br>
+            <textarea id="content" class="mt-2" cols="30" rows="10" v-model="content" placeholder="내용을 작성해주세요!"></textarea><br>
+          </div>
+          <input type="submit" id="submit" class="my-4 btn-blue submit-btn">
+        </form>
+      </div>
 
-    <div class="search-form col-sm-12 col-lg-6">
-      <!-- 영화 검색 폼 -->
-      <form @submit.prevent="searchMovie">
-        <label for="search">영화 검색 : </label>
-        <input type="text" name="search" id="search" v-model="search">
-        <input type="submit" name="search" id="search" value="검색">
-      </form>
-      <!-- 영화 검색 결과가 나오는 곳 -->
-      <ul>
-        <li v-for="result in searchResult" :key="result.id" @click="toResultMovie(result)">{{ result.title }}</li>
-      </ul>
+      <div class="search-form col-sm-12 col-lg-6">
+        <!-- 영화 검색 폼 -->
+        <form @submit.prevent="searchMovie">
+          <label for="search"><b>영화 검색</b></label><br>
+          <input type="text" name="search" id="search" v-model="search" class="mt-2">
+          <input type="submit" name="search" id="search" value="검색" class="my-4 search-btn">
+        </form>
+        <!-- 영화 검색 결과가 나오는 곳 -->
+        <ul class="search-movies" v-if="searchResult.length > 0">
+          <li v-for="result in searchResult" :key="result.id" @click="toResultMovie(result)">{{ result.title }}</li>
+        </ul>
+      </div>
     </div>
     
     
@@ -126,4 +132,41 @@ export default {
 </script>
 
 <style>
+.together-create-wrap {
+  margin-top: 50px;
+  text-align: center;
+}
+
+.article-form input, .article-form textarea, .search-form input {
+  width: 70%;
+  border-radius: 5px;
+  border: 1px solid #2E8ADF;
+}
+
+.search-form ul {
+  width: 70%;
+  
+  margin: 0 auto;
+  padding: 0;
+  border-radius: 5px;
+  overflow: hidden;
+  border: 1px solid #2E8ADF;
+}
+.search-form li {
+  width: 100%;
+  padding: 5px;
+}
+
+.search-form li:nth-child(2n) {
+  background-color: #D6E7F6;
+}
+
+.search-btn {
+  background-color: #fff;
+  color: #2E8ADF;
+}
+
+.search-btn:hover, .submit-btn:hover {
+  font-weight: bold;
+}
 </style>
